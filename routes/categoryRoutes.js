76587690a3ab8459a4e4ca44ddc2f59254d1fc27
Issah <abagnasalifusasco@gmail.com>
2021-10-01 12:55:36 +0000

    Update categoryRoutes.js

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -9,16 +9,17 @@ import {
 import { runValidation } from '../validators';
 import { categoryCreateValidator } from '../validators/categoryValidators';
 
-import { requireSignin, adminMiddleware } from './../middlewares/index';
+import { requireSignin,isAuth, adminMiddleware } from './../middlewares/index';
 
 const router = express.Router();
 
 router
   .route('/category')
   .post(
+    requireSignin,
+    isAuth,
     categoryCreateValidator,
     runValidation,
-    requireSignin,
     adminMiddleware,
     createCategory,
   );
@@ -27,6 +28,6 @@ router.route('/categories').get(getCategories);
 router.route('/category/:slug').get(getSingleCategory);
 router
   .route('/category/:slug')
-  .delete(requireSignin, adminMiddleware, deleteCategory);
+  .delete(requireSignin,isAuth, adminMiddleware, deleteCategory);
 
 module.exports = router;
