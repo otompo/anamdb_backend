ea668f52a1731d21984eaa0064040d5d78ee7e22
Issah <abagnasalifusasco@gmail.com>
2021-10-01 12:54:57 +0000

    Update blogRoutes.js

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -3,6 +3,7 @@ import {
   requireSignin,
   adminMiddleware,
   authMiddleware,
+  isAuth,
 } from './../middlewares/index';
 import {
   createBlog,
@@ -21,22 +22,26 @@ import {
 
 const router = express.Router();
 
-router.route('/blog').post(requireSignin, createBlog);
+router.route('/blog').post(requireSignin, isAuth, createBlog);
 
 router.route('/blogs').get(listBlogs);
 router
   .route('/blogs/list-blogs-categories-tags')
   .post(listAllBlogsCategoriesTags);
 router.route('/blog/:slug').get(getSingleBlog);
-router.route('/blog/:slug').delete(requireSignin, adminMiddleware, deleteBlog);
-router.route('/blog/:slug').put(requireSignin, adminMiddleware, updateBlog);
+router
+  .route('/blog/:slug')
+  .delete(requireSignin, isAuth, adminMiddleware, deleteBlog);
+router
+  .route('/blog/:slug')
+  .put(requireSignin, isAuth, adminMiddleware, updateBlog);
 router.route('/blog/img/:slug').get(getImage);
 router
   .route('/blogs/listpublished')
-  .get(requireSignin, adminMiddleware, listPublishBlogs);
+  .get(requireSignin, isAuth, adminMiddleware, listPublishBlogs);
 router
   .route('/blogs/listunpublished')
-  .get(requireSignin, adminMiddleware, listUnpublishBlogs);
+  .get(requireSignin, isAuth, adminMiddleware, listUnpublishBlogs);
 // router.route('/blog/related').post(listRelated);
 // router.route('/blogs/search').get(listSearch);
 
@@ -54,10 +59,10 @@ router.route('/:userId/blogs').get(listBlogsByUser);
 
 router
   .route('/blog/publish/:slug')
-  .put(requireSignin, adminMiddleware, publishBlog);
+  .put(requireSignin, isAuth, adminMiddleware, publishBlog);
 
 router
   .route('/blog/unpublish/:slug')
-  .put(requireSignin, adminMiddleware, unpublishBlog);
+  .put(requireSignin, isAuth, adminMiddleware, unpublishBlog);
 
 module.exports = router;
