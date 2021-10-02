741de271fac7f3fe3c037c9c58df4b197aca30fb
Issah <abagnasalifusasco@gmail.com>
2021-10-02 12:02:04 +0000

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -23,26 +23,26 @@ const router = express.Router();
 router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/logout').get(logout);
-router.route('/current-user').get(requireSignin, isAuth, currentUser);
+router.route('/current-user').get(isAuth, currentUser);
 router.route('/userpublicprofile/:username').get(getUserProfile);
 // router.route('/profile/:username').get(authMiddleware, getUserProfile);
-router.route('/profile/:username').put(requireSignin, isAuth, updateProfile);
+router.route('/profile/:username').put(isAuth, updateProfile);
 router
   .route('/profile/password/:username')
-  .put(requireSignin, isAuth, updateUserPassword);
+  .put(isAuth, updateUserPassword);
 
 router
   .route('/make-user-admin/:username')
-  .put(requireSignin, adminMiddleware, makeUserAdmin);
+  .put(adminMiddleware, makeUserAdmin);
 router
   .route('/remove-user-admin/:username')
-  .put(requireSignin, isAuth, adminMiddleware, removeAsAdmin);
+  .put(isAuth, adminMiddleware, removeAsAdmin);
 
 router
   .route('/getadminusers')
-  .get(requireSignin, isAuth, adminMiddleware, getAdminUsers);
+  .get(isAuth, adminMiddleware, getAdminUsers);
 router
   .route('/getmembersusers')
-  .get(requireSignin, isAuth, adminMiddleware, getMembersUsers);
+  .get(isAuth, adminMiddleware, getMembersUsers);
 
 module.exports = router;
